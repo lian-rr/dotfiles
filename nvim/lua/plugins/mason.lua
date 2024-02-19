c33390399d647d06ca738ee2860c4d21a519352e
Lian <nail3105@gmail.com>
2024-02-18 21:41:23 -0500

    Lua_ls added

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,32 +1,47 @@
 return {
-    'williamboman/mason.nvim',
-    cmd = { 'Mason', 'MasonInstall', 'MasonInstallAll', 'MasonUpdate' },
-    config = function()
-       require('mason').setup({
-            ensure_installed = { "lua-language-server" }, -- not an option from mason.nvim
+    {
+        'williamboman/mason.nvim',
+        cmd = { 'Mason', 'MasonInstall', 'MasonInstallAll', 'MasonUpdate' },
+        config = function()
+            require('mason').setup({
+                ensure_installed = { "lua-language-server" }, -- not an option from mason.nvim
 
-          PATH = "skip",
+                ui = {
+                    icons = {
+                        package_pending = " ",
+                        package_installed = "󰄳 ",
+                        package_uninstalled = " 󰚌",
+                    },
 
-          ui = {
-            icons = {
-              package_pending = " ",
-              package_installed = "󰄳 ",
-              package_uninstalled = " 󰚌",
-            },
+                    keymaps = {
+                        toggle_server_expand = "<CR>",
+                        install_server = "i",
+                        update_server = "u",
+                        check_server_version = "c",
+                        update_all_servers = "U",
+                        check_outdated_servers = "C",
+                        uninstall_server = "X",
+                        cancel_installation = "<C-c>",
+                    },
+                },
 
-            keymaps = {
-              toggle_server_expand = "<CR>",
-              install_server = "i",
-              update_server = "u",
-              check_server_version = "c",
-              update_all_servers = "U",
-              check_outdated_servers = "C",
-              uninstall_server = "X",
-              cancel_installation = "<C-c>",
-            },
-          },
-
-          max_concurrent_installers = 10,
-       }) 
-    end
+                max_concurrent_installers = 10,
+           })
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        config = function()
+            require('mason-lspconfig').setup({
+                ensure_installed = { 'lua_ls' }
+            })
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({})
+        end
+    },
 }
