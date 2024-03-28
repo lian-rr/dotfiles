fadb6a1ac6341b66844efa74d8821a17c2bd6c55
Lian <nail3105@gmail.com>
2024-03-28 17:17:00 -0400

    NVIM: vim test pluggin

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,10 +1,14 @@
 return {
-    "vim-test/vim-test",
-    config = function()
-        vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", { desc = "Test Nearest" })
-        vim.keymap.set("n", "<leader>tT", ":TestFile<CR>", { desc = "Test File" })
-        vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", { desc = "Test Suite" })
-        vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", { desc = "Test Last" })
-        vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", { desc = "Test Visit" })
-    end,
+	"vim-test/vim-test",
+	dependencies = {
+		"preservim/vimux",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", { desc = "Test Nearest" })
+		vim.keymap.set("n", "<leader>tT", ":TestFile<CR>", { desc = "Test File" })
+		vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", { desc = "Test Suite" })
+		vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", { desc = "Test Last" })
+		vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", { desc = "Test Visit" })
+		vim.cmd("let test#strategy = 'vimux'")
+	end,
 }
