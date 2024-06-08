3708e6b68eab657fd698ca6113f4cd44d6ae6d49
Lian <nail3105@gmail.com>
2024-06-08 00:50:03 -0400

    Treesitter config and mason

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,24 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				highlight = {
-					enable = true,
-					use_languagetree = true,
-				},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                auto_install = true,
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "go",
+                    "sql",
+                },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    use_languagetree = true,
+                },
 
-				indent = { enable = true },
-			})
-		end,
-	},
+                indent = { enable = true },
+            })
+        end,
+    },
 }
