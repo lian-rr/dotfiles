1679078393bba9f25d19c6e4d9cef9661cdbcb55
Lian <nail3105@gmail.com>
2024-06-14 10:57:22 -0400

    hidden files visibles with telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -37,6 +37,44 @@ return {
                             ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
                         },
                     },
+                    vimgrep_arguments = {
+                        "rg",
+                        "--follow", -- Follow symbolic links
+                        "--hidden", -- Search for hidden files
+                        "--no-heading", -- Don't group matches by each file
+                        "--with-filename", -- Print the file path with the matched lines
+                        "--line-number", -- Show line numbers
+                        "--column", -- Show column numbers
+                        "--smart-case", -- Smart case search
+
+                        -- Exclude some patterns from search
+                        "--glob=!**/.git/*",
+                        "--glob=!**/.idea/*",
+                        "--glob=!**/.vscode/*",
+                        "--glob=!**/build/*",
+                        "--glob=!**/dist/*",
+                        "--glob=!**/yarn.lock",
+                        "--glob=!**/package-lock.json",
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                        -- needed to exclude some files & dirs from general search
+                        -- when not included or specified in .gitignore
+                        find_command = {
+                            "rg",
+                            "--files",
+                            "--hidden",
+                            "--glob=!**/.git/*",
+                            "--glob=!**/.idea/*",
+                            "--glob=!**/.vscode/*",
+                            "--glob=!**/build/*",
+                            "--glob=!**/dist/*",
+                            "--glob=!**/yarn.lock",
+                            "--glob=!**/package-lock.json",
+                        },
+                    },
                 },
                 extensions = {
                     ["ui-select"] = {
