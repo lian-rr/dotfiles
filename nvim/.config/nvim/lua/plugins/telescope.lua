6ae1d6d533360059cdb95a20f72b1af5165e4298
Lian Rodriguez <lian.rodriguez@backmarket.com>
2026-02-13 14:58:25 -0500

    updating telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,124 +1,125 @@
 return {
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "debugloop/telescope-undo.nvim",
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                config = function()
-                    require("telescope").load_extension("fzf")
-                end,
-            },
-            "folke/todo-comments.nvim",
-        },
-        config = function()
-            local telescope = require("telescope")
-            local actions = require("telescope.actions")
-            local transform_mod = require("telescope.actions.mt").transform_mod
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		tag = "v0.2.1",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"debugloop/telescope-undo.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				-- config = function()
+				-- 	require("telescope").load_extension("fzf")
+				-- end,
+			},
+			"folke/todo-comments.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local transform_mod = require("telescope.actions.mt").transform_mod
 
-            local trouble = require("trouble")
+			local trouble = require("trouble")
 
-            -- or create your custom action
-            local custom_actions = transform_mod({
-                open_trouble_qflist = function(prompt_bufnr)
-                    trouble.toggle("quickfix")
-                end,
-            })
+			-- or create your custom action
+			local custom_actions = transform_mod({
+				open_trouble_qflist = function(prompt_bufnr)
+					trouble.toggle("quickfix")
+				end,
+			})
 
-            telescope.setup({
-                defaults = {
-                    path_displays = { "smart" },
-                    mappings = {
-                        i = {
-                            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                            ["<C-j>"] = actions.move_selection_next, -- move to next result
-                            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-                            ["<C-t>"] = require("trouble.sources.telescope").open,
-                        },
-                    },
-                    vimgrep_arguments = {
-                        "rg",
-                        "--follow", -- Follow symbolic links
-                        "--hidden", -- Search for hidden files
-                        "--no-heading", -- Don't group matches by each file
-                        "--with-filename", -- Print the file path with the matched lines
-                        "--line-number", -- Show line numbers
-                        "--column", -- Show column numbers
-                        "--smart-case", -- Smart case search
+			telescope.setup({
+				defaults = {
+					path_displays = { "smart" },
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous, -- move to prev result
+							["<C-j>"] = actions.move_selection_next, -- move to next result
+							["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+							["<C-t>"] = require("trouble.sources.telescope").open,
+						},
+					},
+					vimgrep_arguments = {
+						"rg",
+						"--follow", -- Follow symbolic links
+						"--hidden", -- Search for hidden files
+						"--no-heading", -- Don't group matches by each file
+						"--with-filename", -- Print the file path with the matched lines
+						"--line-number", -- Show line numbers
+						"--column", -- Show column numbers
+						"--smart-case", -- Smart case search
 
-                        -- Exclude some patterns from search
-                        "--glob=!**/.git/*",
-                        "--glob=!**/.idea/*",
-                        "--glob=!**/.vscode/*",
-                        "--glob=!**/build/*",
-                        "--glob=!**/dist/*",
-                        "--glob=!**/yarn.lock",
-                        "--glob=!**/package-lock.json",
-                    },
-                },
-                pickers = {
-                    find_files = {
-                        hidden = true,
-                        -- needed to exclude some files & dirs from general search
-                        -- when not included or specified in .gitignore
-                        find_command = {
-                            "rg",
-                            "--files",
-                            "--hidden",
-                            "--glob=!**/.git/*",
-                            "--glob=!**/.idea/*",
-                            "--glob=!**/.vscode/*",
-                            "--glob=!**/build/*",
-                            "--glob=!**/dist/*",
-                            "--glob=!**/yarn.lock",
-                            "--glob=!**/package-lock.json",
-                        },
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                    ["undo"] = {},
-                },
-            })
+						-- Exclude some patterns from search
+						"--glob=!**/.git/*",
+						"--glob=!**/.idea/*",
+						"--glob=!**/.vscode/*",
+						"--glob=!**/build/*",
+						"--glob=!**/dist/*",
+						"--glob=!**/yarn.lock",
+						"--glob=!**/package-lock.json",
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						-- needed to exclude some files & dirs from general search
+						-- when not included or specified in .gitignore
+						find_command = {
+							"rg",
+							"--files",
+							"--hidden",
+							"--glob=!**/.git/*",
+							"--glob=!**/.idea/*",
+							"--glob=!**/.vscode/*",
+							"--glob=!**/build/*",
+							"--glob=!**/dist/*",
+							"--glob=!**/yarn.lock",
+							"--glob=!**/package-lock.json",
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+					["undo"] = {},
+				},
+			})
 
-            telescope.load_extension("undo")
-            telescope.load_extension("fzf")
-            telescope.load_extension("ui-select")
+			telescope.load_extension("undo")
+			telescope.load_extension("fzf")
+			telescope.load_extension("ui-select")
 
-            -- See `:help telescope.builtin`
-            local builtin = require("telescope.builtin")
+			-- See `:help telescope.builtin`
+			local builtin = require("telescope.builtin")
 
-            vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Find existing buffers" })
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find in all files" })
-            vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find recently opened files" })
-            vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Search Git Files" })
-            vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
-            vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Search Diagnostics" })
-            vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
-            vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = "Telescope undo history" })
+			vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Find existing buffers" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find in all files" })
+			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find recently opened files" })
+			vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Search Git Files" })
+			vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Search Diagnostics" })
+			vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+			vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = "Telescope undo history" })
 
-            vim.keymap.set("n", "<leader>/", function()
-                -- You can pass additional configuration to telescope to change theme, layout, etc.
-                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-                    winblend = 10,
-                    previewer = false,
-                }))
-            end, { desc = "Fuzzily search in current buffer" })
+			vim.keymap.set("n", "<leader>/", function()
+				-- You can pass additional configuration to telescope to change theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "Fuzzily search in current buffer" })
 
-            vim.keymap.set("n", "<leader>frs", function()
-                builtin.live_grep({
-                    grep_open_files = true,
-                    prompt_title = "Live Grep in Open Files",
-                })
-            end, { desc = "Search in Open Files" })
-        end,
-    },
+			vim.keymap.set("n", "<leader>frs", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "Search in Open Files" })
+		end,
+	},
 }
